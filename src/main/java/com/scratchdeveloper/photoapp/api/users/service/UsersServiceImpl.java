cfba266f2d0dc8325d06ffaae513a23b9064e181
praveen <praveen.kumar02@hotmail.com>
2021-04-04 02:31:20 +0530

    generate unique public userID

--- src/main/java/com/scratchdeveloper/photoapp/api/users/service/UsersServiceImpl.java
+++ src/main/java/com/scratchdeveloper/photoapp/api/users/service/UsersServiceImpl.java
@@ -1,12 +1,14 @@
 package com.scratchdeveloper.photoapp.api.users.service;
 
+import java.util.UUID;
+
 import com.scratchdeveloper.photoapp.api.users.shared.UserDto;
 
 public class UsersServiceImpl implements UsersService {
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
-		
+		userDetails.setUserId(UUID.randomUUID().toString());
 		return null;
 	}
 
