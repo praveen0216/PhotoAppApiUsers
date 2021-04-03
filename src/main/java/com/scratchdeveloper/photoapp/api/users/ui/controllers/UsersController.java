91970440a55513f9aeeac7971e20164c847b3701
praveen <praveen.kumar02@hotmail.com>
2021-04-03 23:11:09 +0530

    Implementing create user request model class

--- src/main/java/com/scratchdeveloper/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/com/scratchdeveloper/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,16 +1,30 @@
 package com.scratchdeveloper.photoapp.api.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.scratchdeveloper.photoapp.api.users.ui.model.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/status/check")
 	public String status() {
-		return "Working";
+		return "Working on port "+env.getProperty("local.server.port");
+	}
+	
+	@PostMapping
+	public String createUser(@RequestBody CreateUserRequestModel userDetails) {
+		return "create user method is called";
 	}
 
 }
