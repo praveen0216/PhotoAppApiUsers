047b042983a14ec1896874db02c91a2b1b018cfa
praveen <praveen.kumar02@hotmail.com>
2021-04-05 22:01:38 +0530

    implementing spring data JPA CRUD repository

---
+++ src/main/java/com/scratchdeveloper/photoapp/api/users/data/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.scratchdeveloper.photoapp.api.users.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<UserEntity, Long> {
+
+}
