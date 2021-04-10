6982bb54163ce38832d52d595463d91db5f02425
praveen <praveen.kumar02@hotmail.com>
2021-04-10 21:24:48 +0530

    encrypt User password

--- src/main/java/com/scratchdeveloper/photoapp/api/users/service/UsersServiceImpl.java
+++ src/main/java/com/scratchdeveloper/photoapp/api/users/service/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.scratchdeveloper.photoapp.api.users.data.UserEntity;
@@ -15,23 +16,27 @@ import com.scratchdeveloper.photoapp.api.users.shared.UserDto;
 public class UsersServiceImpl implements UsersService {
 	
 	UsersRepository usersRepository;
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	
 	@Autowired
-	public UsersServiceImpl(UsersRepository usersRepository) {
+	public UsersServiceImpl(UsersRepository usersRepository,BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.usersRepository=usersRepository;
+		this.bCryptPasswordEncoder=bCryptPasswordEncoder;
 	}
 	
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		
 		ModelMapper modelMapper= new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		
 		UserEntity userEntity=modelMapper.map(userDetails, UserEntity.class);
 		//userEntity.setEncryptedPassword(UUID.randomUUID().toString());
-		userEntity.setEncryptedPassword("test");
+		
 		
 		usersRepository.save(userEntity);
 		return modelMapper.map(userEntity, UserDto.class);
