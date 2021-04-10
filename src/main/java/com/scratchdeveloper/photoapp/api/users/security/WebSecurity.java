1136d5f89cecefbeb7d3d7a6cb63c0a366b2a48e
praveen <praveen.kumar02@hotmail.com>
2021-04-10 23:43:30 +0530

    allow only IP address of Spring API Gateway

--- src/main/java/com/scratchdeveloper/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/com/scratchdeveloper/photoapp/api/users/security/WebSecurity.java
@@ -1,6 +1,8 @@
 package com.scratchdeveloper.photoapp.api.users.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,11 +11,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter{
 	
+	private Environment env;
+	
+	@Autowired
+	public WebSecurity(Environment env) {
+		this.env=env;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.headers().frameOptions().disable();
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/users/**").permitAll();
+		http.authorizeRequests().antMatchers("/**").hasIpAddress(env.getProperty("gateway.ip"));
 	}
 
 }
