c868ee2a51e8493c3c984b935d9b0c500beaadff
praveen <praveen.kumar02@hotmail.com>
2021-04-10 16:07:24 +0530

    save user details in database

--- src/main/java/com/scratchdeveloper/photoapp/api/users/service/UsersServiceImpl.java
+++ src/main/java/com/scratchdeveloper/photoapp/api/users/service/UsersServiceImpl.java
@@ -2,13 +2,37 @@ package com.scratchdeveloper.photoapp.api.users.service;
 
 import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.scratchdeveloper.photoapp.api.users.data.UserEntity;
+import com.scratchdeveloper.photoapp.api.users.data.UsersRepository;
 import com.scratchdeveloper.photoapp.api.users.shared.UserDto;
 
+@Service
 public class UsersServiceImpl implements UsersService {
+	
+	UsersRepository usersRepository;
+	
+	@Autowired
+	public UsersServiceImpl(UsersRepository usersRepository) {
+		this.usersRepository=usersRepository;
+	}
+	
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		userDetails.setUserId(UUID.randomUUID().toString());
+		
+		ModelMapper modelMapper= new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		
+		UserEntity userEntity=modelMapper.map(userDetails, UserEntity.class);
+		userEntity.setEncryptedPassword("test");
+		
+		usersRepository.save(userEntity);
 		return null;
 	}
 
--- src/main/java/com/scratchdeveloper/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/com/scratchdeveloper/photoapp/api/users/ui/controllers/UsersController.java
@@ -2,6 +2,8 @@ package com.scratchdeveloper.photoapp.api.users.ui.controllers;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.scratchdeveloper.photoapp.api.users.ui.model.CreateUserRequestModel;
+import com.scratchdeveloper.photoapp.api.users.service.*;
+import com.scratchdeveloper.photoapp.api.users.shared.UserDto;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
 	
+	
+	
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	UsersService usersService;
 	
 	@GetMapping("/status/check")
 	public String status() {
@@ -26,6 +35,12 @@ public class UsersController {
 	
 	@PostMapping
 	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+		ModelMapper modelMapper= new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT); 
+		
+		UserDto userDto=modelMapper.map(userDetails, UserDto.class);
+		
+		usersService.createUser(userDto);
 		return "create user method is called";
 	}
 
